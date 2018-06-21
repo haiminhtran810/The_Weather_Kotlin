bd9c240db49ac7b470a0c590af64bf3e6f264c7c
haiminhtran810 <haiminhtran810@gmail.com>
2018-06-21 10:58:48 +0700

    Add dimens and move develop(defaut)

--- app/src/main/java/home/learn/hmt/the_weather_kotlin/screen/home/HomeActivity.kt
+++ app/src/main/java/home/learn/hmt/the_weather_kotlin/screen/home/HomeActivity.kt
@@ -1,13 +1,24 @@
 package home.learn.hmt.the_weather_kotlin.screen.home
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.arch.lifecycle.ViewModelProviders
 import home.learn.hmt.the_weather_kotlin.R
+import home.learn.hmt.the_weather_kotlin.base.screen.activity.BaseDataLoadActivity
+import home.learn.hmt.the_weather_kotlin.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseDataLoadActivity<ActivityHomeBinding, HomeViewModel>(), HomeNavigator {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_home)
+  override fun initData() {
+    super.initData()
+
+  }
+
+  override fun getLayoutId(): Int {
+    return R.layout.activity_home
+  }
+
+  override fun initViewModel(): HomeViewModel {
+    return ViewModelProviders.of(this).get(HomeViewModel::class.java).apply {
+      navigator = this@HomeActivity
+    }
   }
 }
---
+++ app/src/main/java/home/learn/hmt/the_weather_kotlin/screen/home/HomeNavigator.kt
@@ -0,0 +1,9 @@
+package home.learn.hmt.the_weather_kotlin.screen.home
+
+import home.learn.hmt.the_weather_kotlin.base.navigator.BaseNavigator
+
+/**
+ * Created by hcm-102-0006 on 20/06/2018.
+ */
+interface HomeNavigator : BaseNavigator{
+}
