f32811ca29f597533c8e1d31ce39b3c4de5403aa
haiminhtran810 <haiminhtran810@gmail.com>
2018-06-22 09:32:04 +0700

    Fix bug in create Data

--- app/src/main/java/home/learn/hmt/the_weather_kotlin/data/module/Coord.kt
+++ app/src/main/java/home/learn/hmt/the_weather_kotlin/data/module/Coord.kt
@@ -1,7 +1,5 @@
 package home.learn.hmt.the_weather_kotlin.data.module
 
-import android.os.Parcel
-import android.os.Parcelable
 
 /**
  * Created by hcm-102-0006 on 21/06/2018.
@@ -9,28 +7,4 @@ import android.os.Parcelable
 class Coord(
     var lat : Double,
     var lon : Double
-) : Parcelable {
-  constructor(parcel: Parcel) : this(
-      parcel.readDouble(),
-      parcel.readDouble()) {
-  }
-
-  override fun writeToParcel(parcel: Parcel, flags: Int) {
-    parcel.writeDouble(lat)
-    parcel.writeDouble(lon)
-  }
-
-  override fun describeContents(): Int {
-    return 0
-  }
-
-  companion object CREATOR : Parcelable.Creator<Coord> {
-    override fun createFromParcel(parcel: Parcel): Coord {
-      return Coord(parcel)
-    }
-
-    override fun newArray(size: Int): Array<Coord?> {
-      return arrayOfNulls(size)
-    }
-  }
-}
+)
